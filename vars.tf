5f3bc05e10726cfd37729d155cc51492a1f06614
Bas Kraai <bas@kraai.email>
2022-04-01 14:06:53 +0200

    Feature: added iam account from central aws-iam-account

--- vars.tf
+++ vars.tf
@@ -1,6 +1,20 @@
 variable "tfc_token" {
   type = string
+  sensitive = true
 }
 variable "github_token" {
   type = string
+  sensitive = true
+}
+variable "aws_key" {
+  type = string
+  sensitive = true
+}
+variable "aws_secret" {
+  type = string
+  sensitive = true
+}
+variable "aws_role" {
+  type = string
+  sensitive = true
 }
