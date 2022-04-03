43275afac34f3eef7576ff74e15eab9919808910
Bas Kraai <bas@kraai.email>
2022-04-03 17:22:06 +0200

    Lint: ignore rule that does not apply; Bugfix: hard-privatise repo's

--- repos.tf
+++ repos.tf
@@ -2,7 +2,7 @@ resource "github_repository" "services" {
   for_each = yamldecode(data.aws_s3_object.config.body).services
   name        = "iac-aws-${lower(each.key)}"
   description = "An IaC AWS rep for the service: ${title(replace(each.key,"_"," "))}"
-  visibility = each.value.testing ? "private" : "public"
+  visibility  = "Private"
 
   template {
     owner = yamldecode(data.aws_s3_object.config.body).github.owner
