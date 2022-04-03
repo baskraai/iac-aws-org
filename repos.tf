9d82ef6dfe43feee53593968a0e2ea3657f6d2e4
Bas Kraai <bas@kraai.email>
2022-04-03 17:31:18 +0200

    Linter: disable visibily spec because of deprecate private-key

--- repos.tf
+++ repos.tf
@@ -2,7 +2,7 @@ resource "github_repository" "services" {
   for_each = yamldecode(data.aws_s3_object.config.body).services
   name        = "iac-aws-${lower(each.key)}"
   description = "An IaC AWS rep for the service: ${title(replace(each.key,"_"," "))}"
-  visibility  = "Private"
+  visibility  = "private"
 
   template {
     owner = yamldecode(data.aws_s3_object.config.body).github.owner
